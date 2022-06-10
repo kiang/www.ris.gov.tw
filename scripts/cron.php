da8aa48f727eeea378f485d510192cfa369488ee
kiang <kiange@gmail.com>
2022-06-11 00:37:21 +0800

    cron script

---
+++ scripts/cron.php
@@ -0,0 +1,15 @@
+<?php
+$rootPath = dirname(__DIR__);
+
+$now = date('Y-m-d H:i:s');
+
+exec("cd {$rootPath} && /usr/bin/git pull");
+
+exec("php -q {$rootPath}/scripts/01_fetch_raw.php");
+exec("php -q {$rootPath}/scripts/01_raw2json.php");
+
+exec("cd {$rootPath} && /usr/bin/git add -A");
+
+exec("cd {$rootPath} && /usr/bin/git commit --author 'auto commit <noreply@localhost>' -m 'auto update @ {$now}'");
+
+exec("cd {$rootPath} && /usr/bin/git push origin master");
